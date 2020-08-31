37bf534b42f6b25a47c5974b1a969b6b98f28c28
Kumar Chatur Ranjan <kumarchaturranjan48@gmail.com>
2020-08-31 12:01:56 +0530

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -16,14 +16,28 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-  integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <title>Welcome- <?php echo $_SESSION['username'] ?></title>
 
 </head>
+
 <body>
 
   <?php require "Partials/_nav.php" ?>
-  Welcome <?php echo $_SESSION['username'] ?>
+
+  <div class="container my-4">
+
+    <div class="alert alert-success" role="alert">
+      <h4 class="alert-heading text-center">Welcome <?php echo $_SESSION['username'] ?></h4>
+      <p class="text-center">Hey how are you doing? Welcome to iServe. You are logged in as <?php echo $_SESSION['username']?> <br> Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so
+        that you can see how spacing within an alert works with this kind of content.</p>
+      <hr>
+      <p class="mb-0 text-center">Whenever you need to, be sure to log out and keep yourself secure using this link <a href="/CHATUR_PHP/Proj-1/Logout.php">Link to Logout</a>
+      </p>
+    </div>
+
+  </div>
+
 
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
@@ -34,6 +48,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
     integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
     crossorigin="anonymous"></script>
-    
+
 </body>
+
 </html>
